e1f93668cd1273fed7c1b5e870829ed1c93f7f2f
MostafaFlous <mostafaflous@gmail.com>
2023-02-20 13:47:42 +0200

    tss

---
+++ Test Suites/RegisterNewUserTSs/RegisterNewUserTSs.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>RegisterNewUserTSs</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9a121249-4d52-4ce4-9602-59f84bc1883e</testSuiteGuid>
+</TestSuiteEntity>
