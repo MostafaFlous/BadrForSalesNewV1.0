8da15c73ed149b1b9358c52773d881798a5f9a49
MostafaFlous <mostafaflous@gmail.com>
2023-02-23 11:23:07 +0200

    delete

--- Test Suites/TestSuiteCollections/AddingNewDataTestSuiteCollection.ts
+++ Test Suites/TestSuiteCollections/AddingNewDataTestSuiteCollection.ts
@@ -15,16 +15,6 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/RegisterNewUserTSs/RegisterNewUserTSs</testSuiteEntity>
-      </TestSuiteRunConfiguration>
-      <TestSuiteRunConfiguration>
-         <configuration>
-            <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
-         </configuration>
-         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/addNewClientTSs/AddNewClientsTS</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
